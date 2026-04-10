dd15f48d290b3cd426513e7c454e2c68eebe067d
bernricel <brbmusngi.student@ua.edu.ph>
2026-04-10 20:25:31 +0800

    fix logo

--- src/components/layout/DashboardLayout.jsx
+++ src/components/layout/DashboardLayout.jsx
@@ -6,8 +6,8 @@ import { useResponsiveSidebar } from "../../hooks/useResponsiveSidebar";
 import { getDisplayName } from "../../utils/userName";
 import { ROUTES } from "../../constants/routes";
 
-// Branding asset path. If missing, place your logo at `public/citcls-logo.png`.
-const citLogo = "./public/CIT.png";
+// Branding asset path served from `public`.
+const citLogo = "/CIT.png";
 
 export default function DashboardLayout({
   variant,
@@ -71,7 +71,7 @@ export default function DashboardLayout({
         className={`${sidebarClassName} ${isSidebarOpen ? "is-open" : ""}`}
       >
         <div className={brandClassName}>
-          <img src={citLogo} alt="CIT logo" className="brand-logo" />
+          <img src={citLogo} alt="CITCLS logo" className="brand-logo" />
           <div className="brand-copy">
             <strong>CIT Faculty Attendance</strong>
             <span>{brandSubtitle}</span>
